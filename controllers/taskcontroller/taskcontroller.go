14bbfa9bb0ff236d7d05c7776e7757e090c69f6e
Agus Baha <id.stackdevelopers@gmail.com>
2022-08-05 17:11:31 +0700

    add task controller, task model create and input database

--- controllers/taskcontroller/taskcontroller.go
+++ controllers/taskcontroller/taskcontroller.go
@@ -3,7 +3,12 @@ package taskcontroller
 import (
 	"html/template"
 	"net/http"
+
+	"github.com/agusbaha/go-task/entities"
+	"github.com/agusbaha/go-task/models"
 )
+
+var TaskModel = models.NewTaskModel()
 
 func Index(response http.ResponseWriter, request *http.Request) {
 	temp, err := template.ParseFiles("views/task/task.html")
@@ -14,11 +19,33 @@ func Index(response http.ResponseWriter, request *http.Request) {
 }
 
 func Add(response http.ResponseWriter, request *http.Request) {
-	temp, err := template.ParseFiles("views/task/add.html")
-	if err != nil {
-		panic(err)
+	if request.Method == http.MethodGet {
+		// request view jika method get
+		temp, err := template.ParseFiles("views/task/add.html")
+		if err != nil {
+			panic(err)
+		}
+		temp.Execute(response, nil)
+	} else if request.Method == http.MethodPost {
+		// mengirim data ke database jika method post
+		request.ParseForm()
+
+		var task entities.Task
+		task.TaskDetail = request.Form.Get("DetailTask")
+		task.Deadline = request.Form.Get("Deadline")
+		task.Assigment = request.Form.Get("Assigment")
+		task.Status = request.Form.Get("Status")
+
+		// fmt.Println(task)
+		TaskModel.Create(task)
+		data := map[string]interface{}{
+			"Message": "Data Berhasil Ditambahkan",
+		}
+
+		temp, _ := template.ParseFiles("views/task/add.html")
+		temp.Execute(response, data)
 	}
-	temp.Execute(response, nil)
+
 }
 
 func Edit(response http.ResponseWriter, request *http.Request) {
