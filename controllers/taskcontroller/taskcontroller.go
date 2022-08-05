cd2a0a093415f1b9c3ac545443f7fd0a9a3ea80f
Agus Baha <id.stackdevelopers@gmail.com>
2022-08-05 10:22:33 +0700

    main.go route handler dan port, taskcontroller.go return fmt.Println

---
+++ controllers/taskcontroller/taskcontroller.go
@@ -0,0 +1,22 @@
+package taskcontroller
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Index(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("Its Working")
+}
+
+func Add(response http.ResponseWriter, request *http.Request) {
+
+}
+
+func Edit(response http.ResponseWriter, request *http.Request) {
+
+}
+
+func Delete(response http.ResponseWriter, request *http.Request) {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/agusbaha/go-task/controllers/taskcontroller"
+)
+
+func main() {
+	// Route Handler
+	http.HandleFunc("/", taskcontroller.Index)
+	http.HandleFunc("/taks", taskcontroller.Index)
+	http.HandleFunc("/taks/index", taskcontroller.Index)
+	http.HandleFunc("/task/add", taskcontroller.Add)
+	http.HandleFunc("/task/edit", taskcontroller.Edit)
+	http.HandleFunc("/task/delete", taskcontroller.Delete)
+
+	// Start server
+	http.ListenAndServe(":9000", nil)
 }
